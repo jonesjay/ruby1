636f0ba3ee7cbe767b19fe15d8427d08528c98fe
jonesjay <brianmahita@gmail.com>
2018-05-20 16:24:40 +0300

    new assignment

---
+++ assignment.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main(){
+
+
+ int grade,i;
+ int counter,index;
+ i=10;
+ grade<=100;
+ counter=counter+1;
+ index=index+1;
+
+ printf(" Number of students to record their grades \n");
+// printf("Enter the number of students\n ");
+//  scanf("%d",&grade);
+// 
+// printf("Input (a1) if passed and (a2) if failed\n");
+//
+//
+//
+ for(i=1;i<=10;i++){
+    printf("Enter results\n");
+    scanf("%d," );i;
+
+	if (i==1){
+		counter++;
+	}
+	if (i==2){
+		index++;
+	}
+}
+printf("Students who passed,  \n") &counter ;
+printf("Students who failed,  \n") &index ;
+//
+//if(counter>=8){
+//	printf("Bonus to the instructor gerger");
+brian
+//}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
